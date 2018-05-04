25bd6e58b5e9ce727c620bcbfab721237ee3ee6d
İbrahim Yayla <iyayla95e@gmail.com>
2018-05-04 18:01:43 +0300

    Kriptografi

---
+++ kod.py
@@ -0,0 +1,11 @@
+import hashlib
+a=open("yazi.txt")
+x=a.read()
+a.close()
+y=""
+n=int(input("Yazýyý girin"))
+for i in range(0,n):
+	x=hashlib.md5(x.encode('utf-8')).hexdigest()
+	y=y+x[7]
+print("hex karsilik: ",y)
+print("decimal karsilik: ",int(y,16))
